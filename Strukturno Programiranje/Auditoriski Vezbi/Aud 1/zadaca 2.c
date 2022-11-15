ba3a747cf812325d549ae68d67debe870b130188
boran <117874761+JasBoran@users.noreply.github.com>
2022-11-15 04:51:48 +0100

    Create zadaca 2.c

---
+++ Strukturno Programiranje/Auditoriski Vezbi/Aud 1/zadaca 2.c
@@ -0,0 +1,30 @@
+/*
+Во текстот на програмата додадете го означениот ред:
+--------------------------------------------------
+#include <stdio.h>
+
+int main() {
+    printf("Zdravo, kako si?\n");
+    // dodadi go ovoj red
+    printf("Neshto ne ti se pravi muabet?\n");
+    return 0;
+}
+--------------------------------------------------
+
+Кој е резултатот од извршувањето сега?
+*/
+
+#include <stdio.h>
+
+int main() {
+    printf("Zdravo, kako si?\n");
+    // dodadi go ovoj red
+    printf("Neshto ne ti se pravi muabet?\n");
+    return 0;
+}
+
+/*
+Samo printa "Zdravo, kako si?
+             Neshto ne ti se pravi muabet?"
+*/
+                
