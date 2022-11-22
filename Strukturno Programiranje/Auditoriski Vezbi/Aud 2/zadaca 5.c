9d0cac675b8ba450569882a7013622eb14899f48
boran <117874761+JasBoran@users.noreply.github.com>
2022-11-22 06:10:51 +0100

    Create zadaca 5.c

---
+++ Strukturno Programiranje/Auditoriski Vezbi/Aud 2/zadaca 5.c
@@ -0,0 +1,14 @@
+/*
+Да се напише програма која ќе ги отпечати на екран остатоците при делењето на бројот 19 со 2, 3, 5 и 8.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Ostatok pri delenje so 2 e: %d\n",19%2);
+    printf("Ostatok pri delenje so 3 e: %d\n",19%3);
+    printf("Ostatok pri delenje so 5 e: %d\n",19%5);
+    printf("Ostatok pri delenje so 8 e: %d",19%8);
+    return 0;
+}
